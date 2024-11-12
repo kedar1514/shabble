22042196998d6500405e0b01188b313c431dbf2f
coder-zs-cse <www.zubinshah1886@gmail.com>
2024-11-12 16:27:18 +0530

    menu color fixed

--- src/components/daily/nav/menu.tsx
+++ src/components/daily/nav/menu.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Divider, Title } from '@/components'
+import { useGameSettings } from '@/contexts'
 import React from 'react'
 import { FaHeart, FaQuestion } from 'react-icons/fa'
 import { IoClose } from 'react-icons/io5'
@@ -20,6 +21,8 @@ const Menu: React.FC<MenuProps> = ({
     onShowStatistics,
     onShowHelp,
 }) => {
+    const { settings } = useGameSettings();
+
     const difficultyOptions = [
         { size: 5, label: 'EASY', level: '5 x 5', levelClassName: 'text-green-600' },
         { size: 6, label: 'MEDIUM', level: '6 x 6', levelClassName: 'text-yellow-500' },
@@ -50,7 +53,7 @@ const Menu: React.FC<MenuProps> = ({
 
             {/* Sliding Menu */}
             <div className={`
-                absolute top-0 left-0 h-full w-[300px] bg-gray-50 dark:bg-gray-800 
+                absolute top-0 left-0 h-full w-[300px] bg-[#f6f7f8]
                 transform transition-transform duration-300 ease-in-out z-[1000]
                 ${isOpen ? 'translate-x-0' : '-translate-x-full'}
             `}>
@@ -69,30 +72,31 @@ const Menu: React.FC<MenuProps> = ({
                     <div className=" mt-4">
 
                         {difficultyOptions.map((option, index) => (
-                            <div className='flex gap-2 items-center h-[64px] hover:bg-white cursor-pointer' key={index} onClick={() => onSelectDifficulty(option.size)}>
-                                <span className={`text-[20px] font-bold pl-4 ${option.levelClassName}`}>{option.level}</span>
-                                <span className='text-[25px] font-bold '>{option.label}</span>
-                            </div>
-
-                        ))}
-
-                        <Divider isVertical={false} className='my-4' />
-
-                        {menuItems.map((item, index) => (
-                            <div className='flex gap-1 items-center hover:bg-white dark:hover:bg-gray-700 rounded cursor-pointer' onClick={item.onClick} key={index}>
-                                <div className='pl-3'>{item.icon && <item.icon size={30} />}</div>
-                                <div
-                                    className="w-full text-left p-3 text-[25px] uppercase rounded text-[20px] font-bold"
-                                >
-                                    {item.label}
+                            <div className={`flex gap-2 items-center h-[64px] hover:bg-white cursor-pointer ${option.size === settings.boardSize ? 'bg-gray-200 text-black' : ''}`} key={index} onClick={() => onSelectDifficulty(option.size)}>
+                                {/* <div className='flex gap-2 items-center h-[64px] hover:bg-white cursor-pointer' key={index} onClick={() => onSelectDifficulty(option.size)}> */}
+                                    <span className={`text-[20px] font-bold pl-4 ${option.levelClassName}`}>{option.level}</span>
+                                    <span className='text-[25px] font-bold '>{option.label}</span>
                                 </div>
-                            </div>
+
                         ))}
-                    </div>
+
+                                <Divider isVertical={false} className='my-4' />
+
+                                {menuItems.map((item, index) => (
+                                    <div className='flex gap-1 items-center hover:bg-white dark:hover:bg-gray-700 rounded cursor-pointer' onClick={item.onClick} key={index}>
+                                        <div className='pl-3'>{item.icon && <item.icon size={30} />}</div>
+                                        <div
+                                            className="w-full text-left p-3 text-[25px] uppercase rounded text-[20px] font-bold"
+                                        >
+                                            {item.label}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                 </div>
-            </div>
-        </>
-    )
+                </div>
+            </>
+            )
 }
 
-export default Menu
+            export default Menu
